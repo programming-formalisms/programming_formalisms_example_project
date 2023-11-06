d5e53bd089159c76cc076625ba7073c360bd97d3
richelbilderbeek <richel@richelbilderbeek.nl>
2023-11-06 08:42:21 +0100

    Add empty main

---
+++ main.py
@@ -0,0 +1,35 @@
+"""The Programming Formalisms Example Project.
+
+Project used in the UPPMAX Programming Formalisms course.
+"""
+
+"""
+TODO
+
+import cProfile
+
+from pf_example.testing import (
+    get_datas,
+    get_sorting_functions,
+    get_speed_measurements,
+    save_speed_measurements,
+)
+"""
+
+if __name__ == "__main__":
+    print("Cannor measure speed yet")
+"""
+TODO
+    speed_measurements = get_speed_measurements(
+        datas = get_datas(data_lengths = [2, 4, 6, 8, 10]),
+        functions = get_sorting_functions(),
+    )
+    save_speed_measurements(speed_measurements, "speeds.csv")
+
+    cProfile.run(
+        "get_speed_measurements("
+        "datas = get_datas(data_lengths = [2, 4, 6, 8, 10]), "
+        "functions = get_sorting_functions())",
+    )
+
+"""
