4b044a4a071c9fe907db411ba0e75a8cb0374721
Arvid Söderholm <arvid.soderholm09@gmail.com>
2023-11-17 14:05:04 +0100

    added test #16

--- tests/test_position.py
+++ tests/test_position.py
@@ -1,20 +1,25 @@
-"""Tests all function in src.pf_example.simulation_controller."""
+"""Tests all function in src.pf_example.position."""
 import unittest
 
-from src.pf_example.simulation_controller import (
-    SimulationTerminalController,
+from src.pf_example.position import (
+    Position,
 )
 
 
-class TestSimulationTerminalController(unittest.TestCase):
+class TestPosition(unittest.TestCase):
 
-    """Class to test the code in src.pf_example.simulation_controller."""
+    """Class to test the code in src.pf_example.position."""
 
-    def test_can_create_terminal_controller(self):
-        """#7: Can construct a SimulationTerminalController."""
-        SimulationTerminalController()
+    def test_can_create_position(self):
+        """#15: Can construct a Position."""
+        Position()
         self.assertTrue(1 + 2 == 2 + 1)
 
+    def test_origin_created(self):
+        """#16: A Position can its x and y read."""
+        c = Position()
+        self.assertEqual(c.get_x(), 0.0)
+        self.assertEqual(c.get_y(), 0.0)
     """
     TODO
     def test_is_zero(self):
