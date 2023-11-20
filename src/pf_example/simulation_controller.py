b6e8c8a15aed92a12c00429a7d53f163bc676feb
richelbilderbeek <richel@richelbilderbeek.nl>
2023-11-20 11:51:30 +0100

    Can run for 1 second, update design

--- src/pf_example/simulation_controller.py
+++ src/pf_example/simulation_controller.py
@@ -1,26 +1,31 @@
 """SimulationControllers."""
 
 import atexit
+
 import pygame
+
 
 class SimulationWindowController:
 
     """SimulationController that uses a window."""
 
     def __init__(self):
-        """Constructor"""
+        """Construct a SimulationWindowController."""
         pygame.init()
         self._screen = pygame.display.set_mode((1280, 720))
         self._clock = pygame.time.Clock()
-        atexit.register(self.cleanup)
+        atexit.register(self._cleanup)
 
-    def start(self):
-        self._screen.fill("purple")
-        pygame.display.flip()
-        self._clock.tick(60) 
+    def run(self):
+        """Show the window until it is closed."""
+        frames_per_sec = 60
+        for _ in range(0, frames_per_sec): # Run for 1 second
+            self._screen.fill("purple")
+            pygame.display.flip()
+            self._clock.tick(frames_per_sec) 
 
-    def cleanup(self):
-        """Destructor"""
+    def _cleanup(self):
+        """Destroy a SimulationWindowController."""
         pygame.quit()
 
 
