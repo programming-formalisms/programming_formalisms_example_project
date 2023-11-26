2576765bbfdbc7f77d2273c4a3e21f8bd9c5fb43
richelbilderbeek <richel@richelbilderbeek.nl>
2023-11-26 17:03:52 +0100

    A TerminalController is a type of Controller, fix #51

--- src/pf_example/simulation_terminal_controller.py
+++ src/pf_example/simulation_terminal_controller.py
@@ -1,5 +1,10 @@
 """SimulationTerminalController."""
 
-class SimulationTerminalController:
+from src.pf_example.simulation_controller import (
+    SimulationController,
+)
+
+
+class SimulationTerminalController(SimulationController):
 
     """Terminal Controller of Simulation."""
