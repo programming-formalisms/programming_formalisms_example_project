d5f73a38feb8f7db38b1808c1c1ffa891f5f3372
richelbilderbeek <richel@richelbilderbeek.nl>
2023-11-26 14:00:15 +0100

    Add Speed, fix #27

--- tests/test_speed.py
+++ tests/test_speed.py
@@ -11,5 +11,5 @@ class TestSpeed(unittest.TestCase):
     """Class to test the code in src.pf_example.speed."""
 
     def test_can_create_speed(self):
-        """#28: Can construct a Speed."""
+        """#27: Can construct a Speed."""
         Speed()
